dc81972f3c859fbf1833659bcb7c65539d87d53b
Stefan Müller <s.mueller@it.kls-glt.de>
2017-02-17 11:05:06 +0100

    Index with blank window

--- src/gui/index.rs
+++ src/gui/index.rs
@@ -1,6 +1,46 @@
+use errors::*;
+use gdk::enums::key;
+use glib;
+use glib::Value;
+use gobject_sys;
+use gtk_sys;
+use gtk;
+use gtk::prelude::*;
+use macros;
+use xmz_server::*;
 
+use glib::translate::ToGlibPtr;
 
 
 pub fn launch() {
-    println!("Launch index");
+    if gtk::init().is_err() {
+        println!("Failed to initalize GTK.");
+
+        ::std::process::exit(1);
+    }
+    // Disable Animationen
+    // http://stackoverflow.com/questions/39271852/infobar-only-shown-on-window-change/39273438#39273438
+    // https://gitter.im/gtk-rs/gtk?at=57c8681f6efec7117c9d6b5e
+    unsafe{
+        gobject_sys::g_object_set (gtk_sys::gtk_settings_get_default() as *mut gobject_sys::GObject,
+        "gtk-enable-animations".to_glib_none().0, 0, 0);
+    }
+
+    let glade_str = include_str!("gui.glade");
+    let builder = gtk::Builder::new();
+    builder.add_from_string(&glade_str);
+
+    let window_main: gtk::Window = builder.get_object("window_main").expect("Could not find 'window_main' in glade file");
+
+    #[cfg(feature = "development")]
+    window_main.connect_key_press_event(move |_, key| {
+        if let key::Escape = key.get_keyval() {
+            gtk::main_quit()
+        }
+        Inhibit(false)
+    });
+
+    window_main.show_all();
+
+    gtk::main();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,15 @@
 #![recursion_limit = "1024"]
 
 #[macro_use] extern crate error_chain;
+extern crate gdk;
+extern crate glib;
+extern crate gobject_sys;
+extern crate gtk_sys;
+extern crate gtk;
+extern crate xmz_server;
 
 pub mod errors;
 pub mod gui;
+#[macro_export] pub mod macros;
 
 use errors::*;
